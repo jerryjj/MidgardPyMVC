29fa75272dd6774204fa34af23925aef20c3b042
Jerry Jalava <jerry.jalava@gmail.com>
2010-05-12 14:51:45 +0300

    Check if we should ignore enabled flag

--- midgardmvc/lib/midgard/auth/decorators.py
+++ midgardmvc/lib/midgard/auth/decorators.py
@@ -9,7 +9,8 @@ def authenticated(method):
     """Decorate methods with this to require that the user be logged in."""
     @functools.wraps(method)
     def wrapper(self, *args, **kwargs):
-        if not config["midgard.authentication.enabled"]:
+        ignore_enabled_flag=False
+        if not config["midgard.authentication.enabled"] and ignore_enabled_flag == False:
             return method(self)
         
         identity = request.environ.get('repoze.who.identity')
