de5cc2dc6b6463ec7f70ee0e34e60cfe8ebc58ee
Jerry Jalava <jerry.jalava@gmail.com>
2010-05-26 15:40:16 +0300

    redirect_to is deprecated

--- midgardmvc/controllers/page.py
+++ midgardmvc/controllers/page.py
@@ -1,7 +1,7 @@
 import logging
 
 from pylons import request, response, session, tmpl_context as c
-from pylons.controllers.util import abort, redirect_to
+from pylons.controllers.util import abort, redirect
 from pylons.i18n.translation import _
 
 from midgardmvc.lib.base import BaseController, render
