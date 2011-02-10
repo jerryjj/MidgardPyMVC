a94ddb8a34f1e2d9d21f84c86d25e3bc5c183858
Jerry Jalava <jerry.jalava@gmail.com>
2011-02-10 18:53:25 +0200

    Typo fix

--- midgardmvc/lib/midgard/middleware.py
+++ midgardmvc/lib/midgard/middleware.py
@@ -63,7 +63,7 @@ class MidgardMiddleware(object):
 
             if identity and identity.has_key("midgard.user"):
                 user = identity.get("midgard.user")
-                user.log_out()
+                user.logout()
 
     def getTimezone(self, tz_str):
     	import pytz
