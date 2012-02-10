290dfeaf715c0f29966cfbce01c0de34b181b272
piotras <piotrek.pokora@gmail.com>
2012-02-10 11:30:42 +0200

    Added midgard_legacy_query_builder

--- midgardmvc/lib/helpers.py
+++ midgardmvc/lib/helpers.py
@@ -18,6 +18,7 @@ import logging
 log = logging.getLogger(__name__)
 
 from gi.repository import GObject, Midgard
+from midgardmvc.lib.midgard.connection import instance as connection_instance
 
 #from midgardmvc.lib.midgard.middleware import MIDGARD as midgard
 
@@ -34,3 +35,33 @@ def flash_warning(message):
     
 def flash_alert(message):
     flash(message, category='error')
+
+class midgard_legacy_query_builder():
+  group = None
+  qs = None
+  
+  def __init__(self, name):
+    storage = Midgard.QueryStorage(dbclass = name)
+    self.qs = Midgard.QuerySelect(connection = connection_instance.connection, storage = storage)
+    self.qs.toggle_read_only(False)
+
+  def add_constraint(self, name, operator, value):
+    if self.group is None:
+      self.group = Midgard.QueryConstraintGroup(grouptype = "AND")
+    self.group.add_constraint(
+      Midgard.QueryConstraint(
+        property = Midgard.QueryProperty(property = "name"),
+        operator = operator,
+        holder = Midgard.QueryValue.create_with_value(value)
+      )
+    )
+
+  def get_query_select(self):
+    return self.qs
+
+  def execute(self):
+    if self.group is not None:
+      self.qs.set_constraint(self.group)
+    self.qs.execute()
+    return self.qs.list_objects()
+
